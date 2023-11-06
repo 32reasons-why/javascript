f7430d4d2b7f7af27b7c4d8ff02e5ce6397195c2
Sunnyboy Chilwana <chilwanecorp@gmail.com>
2023-11-06 10:56:24 +0200

    array push

--- arrays.js
+++ arrays.js
@@ -1,6 +1,9 @@
 let courses = ["Anguler", "Javascript", "Html", "Java"];
 
+//let lnrs = new Array(8);
+
 for(let i = 0; i < courses.length; i++){
 
+    courses[i] = i;
     console.log(courses[i]);
 }
--- forloop.js
+++ forloop.js
@@ -1,15 +1,20 @@
 
 
-const subs = ["CompScience", "SEngineering", "Database", "IT"];
+let subs = ["CompScience", "SEngineering", "Database", "IT"];
 
 let i=0;
 let len = subs.length;
 let devops = " ";
 
+subs.push("gjh");
+
 for(;i < len; ){
     devops = devops + subs[i] + ' ' ;
 
     i++;
+    
 }
 
-console.log(devops);
+
+
+console.log(subs);
