3f27a88df81efd1174ca8aedaab72096da1255d0
Sunnyboy Chilwana <chilwanecorp@gmail.com>
2023-11-07 11:39:35 +0200

    Setters

---
+++ gettersAndSetters.js
@@ -0,0 +1,24 @@
+
+
+
+const Student ={
+    //properties
+    firstName: "Bmw",
+    Age: 18,
+
+    get getName(){
+        return this.firstName;
+    },
+
+    get getDetails(){
+        return [this.firstName, this.Age];
+    },
+
+    set setName(name){
+        this.name = name;
+    }
+
+
+}
+
+console.log(Student.getDetails);
